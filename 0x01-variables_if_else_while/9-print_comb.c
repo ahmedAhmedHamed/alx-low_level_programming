fb4fdd7adef78b72c3afbbb8d1aec40690e4a6b5
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-02-17 05:36:16 -0800

    s

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - hello
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+int x;
+x = 48;
+while (x < 58)
+{
+putchar(x++);
+if(x != 58)
+putchar(44);
+}
+putchar(10);
+return (0);
+}
