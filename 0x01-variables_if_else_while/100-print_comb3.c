bb698c8fe17f4a21543aceffa52d8aaa0c13c53b
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-02-17 05:50:57 -0800

    s

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - hello
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+int i;
+int j;
+for(i = 48; i<58; i++)
+{
+for(j = 48; j<58; j++)
+{
+if (i >= j)
+continue;
+putchar(i);
+putchar(j);
+if (i == 56 && j == 57)
+continue;
+putchar(44);
+putchar(32);
+}
+}
+}
