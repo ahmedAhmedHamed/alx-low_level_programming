4b378fe681e86e9d671271c711175d04ac4c5b4a
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-02-17 06:10:06 -0800

    s

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,11 +10,11 @@ int main(void)
 int i;
 int j;
 int k;
-for (i = 48; i<58; i++)
+for (i = 48; i < 58; i++)
 {
-for (j = 48; j<58; j++)
+for (j = 48; j < 58; j++)
 {
-for (k = 48; k<58; k++)
+for (k = 48; k < 58; k++)
 {
 if (i >= j || j >= k)
 continue;
@@ -28,5 +28,6 @@ putchar(32);
 }
 }
 }
+putchar(10);
 return (0);
 }
