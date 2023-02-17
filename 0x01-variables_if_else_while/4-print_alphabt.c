08f3eea7efaa6c8d843ab888b18a6f25e24ab91e
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-02-17 04:51:50 -0800

    s

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - hello
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+int x = 96;
+while (x < 97 + 26)
+x++;
+if (x == 112||x == 101)
+continue;
+putchar(x);
+return (0);
+}
