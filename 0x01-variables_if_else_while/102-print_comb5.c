076c9c1261782ee5c4c0c2b555ce9d722e6c4c08
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-02-17 06:56:11 -0800

    s

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,16 +15,23 @@ int count;
 count = 58;
 for (i = 48; i < count; i++)
 {
-    for (j = 48; j < count; j++)
-    {
-        for (k = i+1; k < count; k++)
-        {
-            for (p = j+1; p < count; p++)
-            {
-                printf("%d%d %d%d, ",i,j,k,p);
-            }   
-        }   
-    }
+for (j = 48; j < count; j++)
+{
+for (k = i+1; k < count; k++)
+{
+for (p = j+1; p < count; p++)
+{
+putchar(i);
+putchar(j);
+putchar(k);
+putchar(p);
+if (i == 57 && j == 56 && k == 57 && p == 57)
+continue;
+putchar(32);
+putchar(44);
+}   
+}   
+}
 }
 putchar(10);
 return(0);
