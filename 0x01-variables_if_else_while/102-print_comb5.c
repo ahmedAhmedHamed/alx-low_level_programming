215682d6d6e5a4e2a7c36c25d02a0e4db3f793a9
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-02-17 06:34:57 -0800

    s

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - hello
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+int i;
+int j;
+int k;
+int p;
+int count = 10;
+for (i = 0; i < count; i++)
+{
+    for (j = 0; j < count; j++)
+    {
+        for (k = 0; k < count; k++)
+        {
+            for (p = 0; p < count; p++)
+            {
+                if (i > k || j >= p) continue;
+                printf("%d%d %d%d, ",i,j,k,p);
+            }
+            
+        }
+        
+    }
+    
+}
+}
