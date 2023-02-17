6b9625e443fda426b08e16c18b6f5f0635e9d029
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-02-17 06:08:45 -0800

    s

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - hello
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+int i;
+int j;
+int k;
+for (i = 48; i<58; i++)
+{
+for (j = 48; j<58; j++)
+{
+for (k = 48; k<58; k++)
+{
+if (i >= j || j >= k)
+continue;
+putchar(i);
+putchar(j);
+putchar(k);
+if (i == 55 && j == 56 && k == 57)
+continue;
+putchar(44);
+putchar(32);
+}
+}
+}
+}
