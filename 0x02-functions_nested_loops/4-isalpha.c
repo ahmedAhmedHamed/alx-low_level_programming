6482ab634e61eea6a31948f83c0a16f576718c0d
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-02-21 05:53:34 -0800

    s

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,10 @@
 /**
  * _isalpha - checks if it is alphabetic
+ * @c: potato
  *
  * Return: different
  */
 int _isalpha(int c)
 {
-return (!(c > 90 && (c < 97 || c > 122)) || (c < 97 && (c < 65 || c > 90)));
+return (!((c > 90 && (c < 97 || c > 122)) || (c < 97 && (c < 65 || c > 90))));
 }
