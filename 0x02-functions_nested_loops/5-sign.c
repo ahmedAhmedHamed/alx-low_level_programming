f8e9cdb890b066d9bf7781c06bdbf461626ff971
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-02-21 05:34:40 -0800

    s

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+/**
+ * print_sign - hello
+ *
+ * Return: different always
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+_putchar('0');
+return (0);
+}
