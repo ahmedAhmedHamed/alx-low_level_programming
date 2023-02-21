a02dcbb43f0cd701cfa3af67bd62daa7c8e33ed0
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-02-21 08:23:05 -0800

    s

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,10 +14,10 @@ if (c >= 0)
 _putchar(c % 10 + 48);
 return (c % 10);
 }
-if (c == -2147483646)
+if (c == -2147483648)
 {
-_putchar(6 + '0');
-return (6);
+_putchar(8 + '0');
+return (8);
 }
 k = ~c + 1;
 _putchar(k % 10 + 48);
