0a723926b47deda0856a417c4aedbff850bc83e1
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-02-21 07:29:46 -0800

    s

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,12 @@
 /**
- * _abs - absolute value baby 
+ * _abs - absolute value baby
  *
- * @c: hey 
+ * @c: hey
+ * Return: absolute value
  */
 int _abs(int c)
 {
+if (c < 0)
 return (~c + 1);
+return (c);
 }
