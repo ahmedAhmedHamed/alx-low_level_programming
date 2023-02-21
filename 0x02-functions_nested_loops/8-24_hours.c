4776549271802349b9e80a2bf11acd8c25171057
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-02-21 07:46:26 -0800

    s

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,17 +10,11 @@ int j;
 for (i = 0; i <24; i++)
 for (j = 0; j < 60; j++)
 {
-int i;
-int j;
-for (i = 0; i <24; i++)
-for (j = 0; j < 60; j++)
-{
 _putchar((i / 10) + 48);
 _putchar((i % 10) + 48);
 _putchar(':');
 _putchar((j / 10) + 48);
 _putchar((j % 10) + 48);
 _putchar('\n');
-}
 }
 }
