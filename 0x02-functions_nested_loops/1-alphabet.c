52ba199e881bcc7c3a4fbfce60c9efeeb0723ff6
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-02-21 04:54:37 -0800

    s

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,14 +10,3 @@ for(i = 97; i < 123; i++)
 _putchar((char) i);
 _putchar('\n');
 }
-
-/**
- * main - hello
- *
- * Return: 0 always
- */
-int main(void)
-{
-print_alphabet();
-return (0);
-}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - helo
+ */
+void print_alphabet_x10(void)
+{
+int j;
+int i;
+for(i = 0; i < 9; i++)
+{
+for(j = 97; j < 123; j++)
+_putchar((char) j);
+}
+}
