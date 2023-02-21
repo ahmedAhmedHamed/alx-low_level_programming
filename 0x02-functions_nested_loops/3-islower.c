3fa569e2454c988a73f8127503ef5b85cf768e29
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-02-21 05:36:11 -0800

    s

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * _islower - hello
  *
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,5 @@
-#ifndef main
-#define main
 int _putchar(char c);
 void print_alphabet(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-#endif
