2050d88f3dd60762768fdac093c150928addd225
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-02-21 18:23:18 -0800

    s

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_times_table - whatsup
+ *
+ * @n: number
+ */
+void print_times_table(int n)
+{
+int i;
+int j;
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (i * j < 10)
+{
+_putchar((i * j) + 48);
+}
+else if (i * j > 9)
+{
+_putchar(((i * j) / 10) + 48);
+_putchar(((i * j) % 10) + 48);
+}
+if (j == 9)
+continue;
+_putchar(',');
+_putchar(' ');
+if (i * (j + 1) <= 9)
+_putchar(' ');
+}
+_putchar('\n');
+}
+}
