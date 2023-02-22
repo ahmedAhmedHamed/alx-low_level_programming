c0ca1afa00469483c533f40c5f6d95f76bdf5365
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-02-21 18:17:23 -0800

    s

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,51 +1,32 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_to_98 - prints to 98
+ * print_to_98 - prints alot
+ *
  * @n: number
  */
 void print_to_98(int n)
 {
 int i;
-int x;
-x = 0;
+if (n <= 98)
 for (i = n; i <= 98; i++)
 {
-if (i < 0)
-_putchar('-');
-if (i < -99 && x == 0)
+printf("%d", i);
+if (i != 98)
 {
-_putchar((~i + 1) / 10 / 10 + '0');
-_putchar((~i + 1) / 10 % 10 + '0');
-_putchar((~i + 1) % 10 + '0');
-x = 1;
+printf(", ");
 }
-if (i < -9 && x == 0)
+}
+else
 {
-_putchar((~i + 1) / 10 + '0');
-_putchar((~i + 1) % 10 + '0');
-x = 1;
-}
-if (i < 0 && x == 0)
+for (i = n; i >= 98; i--)
 {
-_putchar((~i + 1) % 10 + '0');
-x = 1;
-}
-if (i > 9 && x == 0)
+printf("%d", i);
+if (i != 98)
 {
-_putchar(i / 10 + '0');
-_putchar(i % 10 + '0');
-x = 1;
+printf(", ");
 }
-if (x == 0)
-_putchar(i + '0');
-x = 0;
-if (i == 98)
-{
-_putchar('\n');
-continue;
 }
-_putchar(',');
-_putchar(' ');
 }
+printf("\n");
 }
