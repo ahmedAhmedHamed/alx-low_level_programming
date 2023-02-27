7f7d05ffe306be3b45c64826fc10970c2b32787c
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-02-27 09:24:06 -0800

    s

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+/**
+ * rev_string - helo
+ * @s: s
+ */
+void rev_string(char *s)
+{
+char* s = "hello";
+char * p;
+p = s;
+int k;
+k = 0;
+while (*p++ != '\0')
+k++;
+int e;
+e = k / 2;
+p--;
+while (e != 0)
+{
+char temp;
+temp = *s;
+*s = *p;
+*p = temp;
+p--;
+s++;
+e--;
+}
+}
