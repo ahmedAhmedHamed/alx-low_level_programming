502f01413169973058bb7c95cabbbb9cacc62c1f
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-02 10:12:33 -0800

    s

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * _strcmp - helo
+ * @s1: n
+ * @s2: n2
+ * Return: helo
+ */
+int _strcmp(char *s1, char *s2)
+{
+int c1;
+int c2;
+int n1;
+int n2;
+n1 = 0;
+n2 = 0;
+c1 = 0;
+c2 = 0;
+while (*s1 != '\0' && *s2 != '\0')
+{
+n1 += *s1;
+n2 += *s2;
+c1++;
+c2++;
+s1++;
+s2++;
+}
+while (*s1 != '\0')
+{
+n1 += *s1++;
+c1++;
+}
+while (*s2 != '\0')
+{
+c2++;
+n2 += *s2++;
+}
+if (c1 > c2)
+return (1);
+else if (c1 < c2)
+return (-1);
+else if (n1 > n2)
+return (1);
+else if (n2 > n1)
+return (-1);
+else
+return (0);
+}
