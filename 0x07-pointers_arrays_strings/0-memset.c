e1512e1b03225d211bc4fddcea78d8edb23f6e48
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-06 10:33:43 -0800

    s

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - helo
+ * @s: char*
+ * @b: b
+ * @n: n
+ * Return: char*
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+int k = 0;
+while (n--)
+{
+s[k++] = b;
+}
+return s;
+}
