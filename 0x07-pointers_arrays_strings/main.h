e0b97306a817bde99ce9f3bb135a43c7e7a852d4
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-06 13:02:01 -0800

    s

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,3 +4,5 @@ char *_strchr(char *s, char c);
 int inStr(char c, char* str);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
