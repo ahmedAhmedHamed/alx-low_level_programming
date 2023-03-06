6fa354bcc71de367065ef29dc87c1dbfc0c048be
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-06 10:44:46 -0800

    s

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _memcpy - helo
+ * @dest: s
+ * @src: s
+ * @n: s
+ * Return: s
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int k = 0;
+while (n--)
+dest[k] = src[k++];
+return (dest);
+}
