e7339038f7dfe133f774f21068307e5bbdcb0ac5
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-06 10:53:35 -0800

    s

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * _strchr - helo
+ * @s: char*
+ * @c: s
+ * Return: char* or null
+ */
+char *_strchr(char *s, char c)
+{
+    while (*s != '\0')
+    {
+        if (*s == c)
+            return (s);
+        s++;
+    }
+    return (NULL);
+}
