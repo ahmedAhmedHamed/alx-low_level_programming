40298913ddef67fc561bf6db5ea0570fff8545cc
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-08 07:56:43 -0800

    s

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * myIsPalindrome - hello
  * @s: s
@@ -8,11 +9,23 @@
  */
 int myIsPalindrome(char *s, int x, int len)
 {
-    if (s[x] != s[len - x])
-        return 0;
-    if (x >= len / 2)
-        return 1;
-    return (myIsPalindrome(s, x + 1, len));
+if (s[x] != s[len - x])
+return (0);
+if (x >= len / 2)
+return (1);
+return (myIsPalindrome(s, x + 1, len));
+}
+/**
+ * stringLength - hello
+ * @s: s
+ * @n: s
+ * Return: s
+ */
+int stringLength(char *s, int len)
+{
+if (s[len] == '\0')
+return (0);
+return (1 + stringLength(s, len + 1));
 }
 
 /**
@@ -22,8 +35,7 @@ int myIsPalindrome(char *s, int x, int len)
  */
 int is_palindrome(char *s)
 {
-int k = 0;
-while (s[k] != '\0')
-    k++;
+int k;
+k = stringLength(s, 0);
 return (myIsPalindrome(s, 0, k - 1));
 }
