8f1d3632e784b050f7d744ad5fb5e6f07f67a688
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-10 14:46:51 -0800

    s

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+/**
+ * main - main
+ * @argc: argc
+ * @argv: argv
+ * Return: 1
+ */
+int main(int argc, char *argv[])
+{
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+printf("%d\n", (argv[1] - '0') * (argc[2] - '0'));
+return (0);
+}
