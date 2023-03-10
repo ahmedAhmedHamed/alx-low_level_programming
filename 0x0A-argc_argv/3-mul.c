af120dfa2a9749f6792c381a4ca3584177d78faf
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-10 15:38:48 -0800

    s

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,11 +7,21 @@
  */
 int main(int argc, char *argv[])
 {
+int num1;
+int num2;
 if (argc < 3)
 {
 printf("Error\n");
 return (1);
 }
-printf("%d\n", (*argv[1] - '0') * (*argv[2] - '0'));
+if (argv[1][1] != '\0')
+num1 = (argv[1][1] - '0') * -1;
+else
+num1 = *argv[1] - '0';
+if (argv[2][1] != '\0')
+num2 = (argv[2][1] - '0') * -1;
+else
+num2 = *argv[2] - '0';
+printf("%d\n", num1 * num2);
 return (0);
 }
