ff31e5656ef525ebb1036939cb111d709e64fe3d
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-10 17:49:33 -0800

    s

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,21 +7,43 @@
  */
 int main(int argc, char *argv[])
 {
-int num1;
-int num2;
+int num1 = 0;
+int num2 = 0;
+int mult1 = 1;
+int mult2 = 1;
+int i;
+int j;
+int flag1 = 0;
+int flag2 = 0;
 if (argc < 3)
 {
 printf("Error\n");
 return (1);
 }
-if (argv[1][1] != '\0')
-num1 = (argv[1][1] - '0') * -1;
-else
-num1 = *argv[1] - '0';
-if (argv[2][1] != '\0')
-num2 = (argv[2][1] - '0') * -1;
-else
-num2 = *argv[2] - '0';
+for (i = 0; argv[1][i] != '\0'; i++);
+for (j = 0; argv[1][j] != '\0'; j++);
+i--;
+j--;
+while (i >= 0)
+{
+if (argv[2][i] == '-')
+{
+num1 *= -1;
+break;
+}
+num1 += (argv[1][i--] - '0') * mult1;
+mult1 *= 10;
+}
+while (j >= 0)
+{
+if (argv[2][j] == '-')
+{
+num2 *= -1;
+break;
+}
+num2 += (argv[2][j--] - '0') * mult2;
+mult2 *= 10;
+}
 printf("%d\n", num1 *num2);
 return (0);
 }
