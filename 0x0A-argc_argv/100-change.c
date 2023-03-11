0ef55d7fca65fb823df60f471d9060757074060a
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-10 18:56:34 -0800

    s

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,26 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - helo
- * @argc: s
- * @argv: s
+ * calculate - hello
+ * @num1: s
  * Return: s
  */
-int main(int argc, char *argv[])
+int calculate(int num1)
 {
-int num1;
 int finNum = 0;
-num1 = atoi(argv[1]);
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
-}
-if (num1 < 0)
-{
-printf("0\n");
-return (0);
-}
 while (num1 != 0)
 {
 if (num1 >= 25)
@@ -50,6 +37,31 @@ num1 -= 1;
 finNum++;
 }
 }
+return (finNum);
+}
+
+/**
+ * main - main
+ * @argc: s
+ * @argv: s
+ * Return: s
+ */
+int main(int argc, char *argv[])
+{
+int num1;
+int finNum = 0;
+num1 = atoi(argv[1]);
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+if (num1 < 0)
+{
+printf("0\n");
+return (0);
+}
+finNum = calculate(num1);
 printf("%d\n", finNum);
 return (0);
 }
