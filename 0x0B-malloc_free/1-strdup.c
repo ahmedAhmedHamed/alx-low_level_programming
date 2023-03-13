cbc03ea141c27546c5794de1f5419b857c2f49dc
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-13 16:18:21 -0700

    s

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strdup - hello
+ * @str: s
+ * Return: s
+ */
+char *_strdup(char *str)
+{
+int k = 0;
+char * myStr;
+if (str == 0)
+return (0);
+
+while (str[k] != '\0')
+k++;
+
+myStr = malloc(sizeof(char) * k);
+while (k--)
+myStr[k] = str[k];
+myStr[k] = str[k];
+
+return (myStr);
+}
