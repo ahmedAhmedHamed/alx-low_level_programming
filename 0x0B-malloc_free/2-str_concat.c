505f0a58a08aac48d7fb9f103a4a79b111600702
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-13 16:58:21 -0700

    s

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,41 +8,44 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    int i = 0;
-    int j = 0;
-    int k = 0;
-    char *str;
+int i = 0;
+int j = 0;
+int k = 0;
+char *str;
 
-    if (s1 == 0 && s2 == 0)
-        return (0);
+if (s1 == 0 && s2 == 0)
+return (0);
 
-    while (s1 != 0 && s1[i] != '\0')
-    {
-        i++;
-    }
+while (s1 != 0 && s1[i] != '\0')
+{
+i++;
+}
 
-    while (s2 != 0 && s2[j] != '\0')
-    {
-        j++;
-    }
+while (s2 != 0 && s2[j] != '\0')
+{
+j++;
+}
 
-    str = malloc(i + j + 1);
+str = malloc(i + j + 1);
 
-    if (str == 0)
-        return (0);
+if (str == 0)
+return (0);
 
-    while(k <= i)
-    {
-        str[k] = s1[k];
-        k++;
-    }
-    while (k <= i + j)
-    {
-        str[k + i] = s2[k];
-        k++;
-    }
+while(k < i)
+{
+str[k] = s1[k];
+k++;
+}
 
-    str[k] = '\0';
+k = 0;
 
-    return (str);
+while (k <= i + j)
+{
+str[k + i] = s2[k];
+k++;
+}
+
+str[k] = '\0';
+
+return (str);
 }
