6ee6bc1bb6181c4e13368756ffec63bc47b707d9
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-14 11:48:56 -0700

    s

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,41 +8,41 @@
  */
 int **alloc_grid(int width, int height)
 {
-    int i = 0;
-    int j = 0;
-    int **s;
-    if (width == 0 || height == 0)
-        return (0);
+int i = 0;
+int j = 0;
+int **s;
+if (width == 0 || height == 0)
+return (0);
 
-    s = malloc(width);
-    if (s == 0)
-    {
-        free(s);
-        return (0);
-    }
+s = malloc(sizeof (int *) * height);
+if (s == 0)
+{
+free(s);
+return (0);
+}
 
-    for (i = 0; i < width; i++)
-    {
-        s[i] = malloc(height);
+for (i = 0; i < height; i++)
+{
+s[i] = malloc(sizeof (int) * width);
 
-        if (s[i] == 0)
-        {
-            while (i != -1)
-            {
-                free(s[i]);
-                i--;
-            }
-            free(s);
-            return (0);
-        }
+if (s[i] == 0)
+{
+while (i != -1)
+{
+free(s[i]);
+i--;
+}
+free(s);
+return (0);
+}
 
-        if (s[i] == 0)
-            return (0);
-        for (j = 0; j < height; j++)
-        {
-            s[i][j] = 0;
-        }
-    }
+if (s[i] == 0)
+return (0);
+for (j = 0; j < width; j++)
+{
+s[i][j] = 0;
+}
+}
 
-    return (s);
+return (s);
 }
