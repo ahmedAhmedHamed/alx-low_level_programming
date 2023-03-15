2999b17b631f7e7a48588e6f409063d9b3c17a78
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-15 06:33:12 -0700

    s

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -14,7 +14,7 @@ int k = 0;
 int counter = 0;
 char *str;
 if (ac == 0 || av == 0 || av[0] == 0)
-return 0;
+return (0);
 for (i = 0; i < ac; i++)
 {
 while (av[i][j] != '\0')
@@ -30,12 +30,13 @@ str = malloc(counter);
 if (str == 0)
 {
 free(str);
-return 0;
+return (0);
 }
 j = 0;
 for (i = 0; i < ac; i++)
 {
-while(av[i][j] != '\0'){
+while (av[i][j] != '\0')
+{
 str[k] = av[i][j];
 k++;
 j++;
