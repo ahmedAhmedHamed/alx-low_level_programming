6ec1227c8fbdbaa1f8775d90dcf734ab12a16129
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-16 15:17:21 -0700

    s

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _calloc - hello
+ * @nmemb: s
+ * @size: s
+ * Return: s
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+int i;
+char *s;
+if (nmemb == 0 || size == 0)
+return (0);
+s = malloc(nmemb * size);
+if (s == 0)
+return (0);
+for (i= 0; i < nmemb+size; i++)
+s[i] = 0;
+return (s);
+}
