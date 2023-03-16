803ddb0a96b33c9c13a8624064364b34990f84d6
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-16 16:59:30 -0700

    s

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -29,15 +29,14 @@ char *s;
 char *s1;
 if (old_size == new_size)
 return (ptr);
-
 if (ptr == 0)
 {
 s = malloc(new_size);
 if (s == 0)
-    return (0);
+return (0);
 return (s);
 }
-else if(new_size == 0)
+else if (new_size == 0)
 {
 free(ptr);
 return (0);
@@ -45,9 +44,7 @@ return (0);
 else
 s = malloc(new_size);
 if (s == 0)
-{
-    return (0);
-}
+return (0);
 s1 = ptr;
 for (i = 0; i < getMin(old_size, new_size); i++)
 s[i] = s1[i];
