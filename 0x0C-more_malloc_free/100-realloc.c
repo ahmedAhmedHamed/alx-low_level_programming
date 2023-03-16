3561bef9faf500d1d5a621405866aa506a96965a
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-16 16:20:55 -0700

    s

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * getMin - hello
+ * @one: s
+ * @two: s
+ * Return: s
+ */
+int getMin(int one, int two)
+{
+    if (one > two)
+        return (two);
+    if (two > one)
+        return (one);
+    return (one);
+}
+
+/**
+ * _realloc - hello
+ * @ptr: s
+ * @old_size: s
+ * @new_size: s
+ * Return: hello
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+int i;
+int min;
+char *s;
+char *s1;
+if (old_size == new_size)
+return (ptr);
+
+if (ptr == 0)
+{
+s = malloc(new_size);
+if (s == 0)
+return (0);
+return (s);
+}
+else if(new_size == 0)
+{
+free(ptr);
+return (0);
+}
+else
+s = malloc(new_size);
+
+s1 = ptr;
+
+for (i = 0; i < getMin(old_size, new_size); i++)
+{
+s[i] = s1[i];
+}
+
+return (s);
+}
