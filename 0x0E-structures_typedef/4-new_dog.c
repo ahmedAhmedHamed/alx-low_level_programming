606b51ebd6cd8e16d90179ba656df4a97088547e
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-20 13:05:23 -0700

    s

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,9 +10,27 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-char *strname = malloc(strlen(name));
-char *strowner = malloc(strlen(owner));
+char *strname = malloc(strlen(name) * sizeof(char));
+if (strname == 0)
+{
+free(strname);
+return (0);
+}
+char *strowner = malloc(strlen(owner) * sizeof(char));
+if (strowner == 0)
+{
+free(strowner);
+free(strname);
+return (0);
+}
 dog_t *myDog = malloc(sizeof(dog_t));
+if (myDog == 0)
+{
+free(strowner);
+free(strname);
+free(myDog);
+return (0);
+}
 strcpy(strname, name);
 strcpy(strowner, owner);
 myDog->name = strname;
