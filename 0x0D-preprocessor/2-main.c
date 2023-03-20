d658d0ec34c3a3e0834d3973b845e82386cbb772
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-20 07:34:21 -0700

    s

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
- * main - main
+ * main - hello
  * Return: exit code
  */
-int main (void)
-{
-printf("%s", __FILE__);
-return(0);
+int main(void) {
+char s[] = __FILE__;
+char *str;
+int i;
+int len = strlen(s);
+int len2;
+int j = 0;
+len--;
+len2 = len;
+while (s[len] != '/')
+len--;
+len++;
+len2++;
+
+
+str = malloc(len2 - len + 1);
+for (i = len; i < len2 && j < len2 - len; i++)
+str[j++] = s[i];
+str[j] = '\0';
+
+printf("%s", str);
+
+free(str);
+return 0;
 }
