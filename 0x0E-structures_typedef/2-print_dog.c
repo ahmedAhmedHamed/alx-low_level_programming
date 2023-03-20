9f56f872f5a09a864e5c63cb9bfbb3b71d679edb
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-20 11:54:16 -0700

    s

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+/**
+ * print_dog - hello
+ * @d: s
+ */
+void print_dog(struct dog *d)
+{
+if (d == 0)
+return;
+printf("Name: ");
+if (d->name != 0)
+printf("%s\n", d->name);
+else
+printf("nil\n");
+printf("Age: ");
+if (d->age == 0)
+printf("nil\n");
+else
+printf("%d\n", d->age);
+printf("Owner: ");
+if (d->owner == 0)
+printf("nil");
+else
+printf("%s\n", d->owner);
+}
