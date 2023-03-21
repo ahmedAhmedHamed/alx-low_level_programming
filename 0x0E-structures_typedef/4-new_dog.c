0ad47be347129aba5613d03c588a213c20a42fba
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-20 17:04:08 -0700

    s

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "dog.h"
+
 /**
  * new_dog - hello
  * @name: s
@@ -11,23 +12,25 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *myDog;
-char *strname = malloc(strlen(name));
-char *strowner = malloc(strlen(owner));
-
-strowner = malloc(strlen(owner));
-strname = malloc(strlen(name));
 myDog = malloc(sizeof(dog_t));
-if (strowner == 0 || strname == 0 || myDog == 0)
+if (myDog == 0)
 {
-free(strowner);
-free(strname);
 free(myDog);
 return (0);
 }
-strcpy(strname, name);
-strcpy(strowner, owner);
-myDog->name = strname;
+myDog->name = malloc(sizeof(char) * strlen(name));
+myDog->owner = malloc(sizeof(char) * strlen(owner));
+
+if (myDog->owner == 0 || myDog->name == 0 || myDog == 0)
+{
+free(myDog->name);
+free(myDog->owner);
+free(myDog);
+return (0);
+}
+
+strcpy(myDog->name, name);
+strcpy(myDog->owner, owner);
 myDog->age = age;
-myDog->owner = strowner;
 return (myDog);
 }
