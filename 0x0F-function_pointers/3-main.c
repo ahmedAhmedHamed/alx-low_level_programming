bd5158f99fa3836ffcb237d9579d568df9265e6f
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-22 16:16:52 -0700

    s

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,32 +1,34 @@
-#include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include "calc.h"
 
 /**
-  * main - ...
-  * @argc: ...
-  * @argv: ...
-  *
-  * Return: ...
-  */
+ * main - check the code
+ * @argc: s
+ * @argv: s
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
-	int (*oprt)(int, int);
-
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	oprt = get_op_func(argv[2]);
-
-	if (!oprt)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
-	return (0);
+int num1;
+int num2;
+int num3;
+num2 = atoi(argv[3]);
+if (argc != 4)
+{
+printf("Error\n");
+return (98);
+}
+if (argv[2][0] != '+' && argv[2][0] != '/' && argv[2][0] != '%' && argv[2][0] != '*' && argv[2][0] != '-' || strlen(argv[2]) != 1)
+{
+printf("Error\n");
+return (99);
+}
+if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+{
+printf("Error\n");
+return (100);
+}
+num1 = atoi(argv[1]);
+num3 = get_op_func(argv[2])(num1, num2);
+printf("%d\n", num3);
+return (0);
 }
