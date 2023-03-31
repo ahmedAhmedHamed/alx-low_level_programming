de633efd22effa7ffc161a21f675426d1234da74
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-30 18:31:55 -0700

    s

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -21,4 +21,6 @@ struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+int _strlen(const char *s);
+list_t *add_node(list_t **head, const char *str);
 #endif
