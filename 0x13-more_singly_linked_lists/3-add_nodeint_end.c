7d07302551b1841d23ab782c79723e4c1313b2cc
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-04-03 14:08:33 -0700

    s

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - hello
+ * @head: s
+ * @n: s
+ * Return: listint_t*
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+listint_t *new;
+if (*head == 0)
+{
+new = malloc(sizeof(listint_t *));
+if (new == 0)
+return (0);
+new->n = n;
+new->next = 0;
+*head = new;
+return (*head);
+}
+return(add_nodeint_end(&(*head)->next, n));
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,6 +17,7 @@ struct listint_s *next;
 #include <stdlib.h>
 #include <string.h>
 #include <stddef.h>
+listint_t *add_nodeint_end(listint_t **head, const int n);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 size_t print_listint(const listint_t *h);
