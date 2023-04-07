88e9f97f455873e153411e2e42378f9ce2f68798
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-04-06 23:43:09 -0700

    solved no.0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * _strlen - helo
+ * @s: number
+ * Return: number
+ */
+int _strlen(const char *s)
+{
+int len;
+len = 0;
+while (*s != '\0')
+{
+s++;
+len++;
+}
+return (len);
+}
+
+/**
+ * binary_to_uint - hello
+ * @b: s
+ * Return: s
+ */
+unsigned int binary_to_uint(const char *b)
+{
+int i;
+int multi = 1;
+unsigned int x = 0;
+int len = _strlen(b);
+for (i = len - 1; i >= 0; i--)
+{
+if (b[i] == '1')
+{
+x += multi;
+}
+multi *= 2;
+}
+return (x);
+}
