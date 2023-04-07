ff206d0b0c491637a719111d8d708ad6b5bf893b
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-04-06 23:55:15 -0700

    solution for 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * myPrintBinary - prints number in binary (doesn't work if number is zero)
+ * @n: number to be converted
+ */
+void myPrintBinary(unsigned long int n)
+{
+if (n == 0)
+{
+return;
+}
+myPrintBinary(n >> 1);
+putchar((n & 1) + '0');
+}
+
+/**
+ * print_binary - driver for myPrintBinary (necessary because of special case of n == 0)
+ * @n: number to be converted
+ */
+void print_binary(unsigned long int n)
+{
+if (n == 0)
+putchar('0');
+else
+myPrintBinary(n);
+}
