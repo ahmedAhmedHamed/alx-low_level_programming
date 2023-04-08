daad42d032106b27f7a1f15c267c29e2985cfbea
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-04-08 11:04:06 -0700

    s

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,34 +1,60 @@
 #include "main.h"
 
 /**
- * read_textfile -
- * @param filename namestring
- * @param letters letters to be copied
+ * _strlen - helo
+ * @s: number
+ * Return: number
+ */
+size_t _strlen(char *s)
+{
+size_t len;
+len = 0;
+while (*s != '\0')
+{
+s++;
+len++;
+}
+return (len);
+}
+
+/**
+ * getMin - gets the minimum of the two numbers
+ * @one: s
+ * @two: s
+ * Return: s
+ */
+int getMin(int one, int two)
+{
+if (one < two)
+return (one);
+return (two);
+}
+
+/**
+ * read_textfile - hello
+ * @filename: namestring
+ * @letters: letters to be copied
  * Return: number of letters copied
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-unsigned int count = 0;
+size_t count = 0;
+size_t check = 0;
+size_t letterscopy = letters;
 FILE *filePtr;
-char *str = 0;
+char str[1000];
 if (filename == 0)
 return (0);
 filePtr = fopen(filename, "r");
 if (filePtr == 0)
 return (0);
-str = malloc(letters * sizeof(char));
-if (str == 0)
-return (0);
-if (fgets(str, letters, filePtr) == 0)
+while (fgets(str, letterscopy, filePtr) != 0 && (letters - count) > 0)
 {
-free(str);
-fclose(filePtr);
+check = write(1, str, getMin(_strlen(str), letters - count));
+if (check != getMin(_strlen(str), letters - count))
 return (0);
+count += check;
 }
 fclose(filePtr);
-count = write(1, str, letters);
-free(str);
-if (count != letters)
-return (0);
 return (count);
 }
