d79a35054423f6e61f227ddcb296ae4fdb87c134
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-04-08 11:46:12 -0700

    s

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -38,12 +38,13 @@ return (two);
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-if (filename == 0)
-return (0);
-int fd = open(filename, O_RDONLY);
 char str[1000];
 int check;
 int printed;
+int fd;
+if (filename == 0)
+return (0);
+fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
 check = read(fd, str, letters);
