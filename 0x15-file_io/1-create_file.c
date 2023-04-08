6d66b3984ea9e2b6078255e4a40627e660df91d6
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-04-08 12:48:54 -0700

    s

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,13 +28,19 @@ int create_file(const char *filename, char *text_content)
 {
 int fd;
 int check;
+
 if (filename == 0)
 return (-1);
-fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0600);
-if (text_content == 0)
-return (1);
+
+fd = open(filename, O_WRONLY | O_CREAT | O_APPEND , 0600);
+
+if (text_content != 0)
 check = write (fd, text_content, _strlen(text_content));
+
+close(fd);
+
 if (check == -1)
 return (-1);
+
 return (1);
 }
