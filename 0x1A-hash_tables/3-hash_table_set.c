786b58516bac097a2e3e6ec0ac4db88e37f084b5
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-06-09 10:26:59 +0300

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,12 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_set - puts key in table
+ * @ht: the hash table
+ * @key: the key
+ * @value: the value
+ * Return: 0 on failure 1 on success
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
@@ -19,7 +26,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht->array[index] != NULL)
 	{
-		while(i < ht->size && ht->array[i] != NULL)
+		while (i < ht->size && ht->array[i] != NULL)
 			i++;
 		if (i == ht->size)
 			return (0);
