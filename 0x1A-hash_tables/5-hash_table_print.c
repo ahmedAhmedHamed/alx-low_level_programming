40e68074ae7b0f07c2796e72e242170537b4fd60
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-06-09 11:37:47 +0300

    fixed

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,10 +11,7 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *printSlave = NULL;
 
 	if (ht == NULL)
-	{
-		printf("{}\n");
 		return;
-	}
 	printf("{");
 	for (i = 0; ht->array != NULL && i < ht->size; i++)
 		if (ht->array[i] != NULL)
