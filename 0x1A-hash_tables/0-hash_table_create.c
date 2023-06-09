a7baf0ff7e2c7b15e23dbc8ff78c64b4020c3b75
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2023-06-09 08:34:36 +0300

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,4 +1,10 @@
 #include "hash_tables.h"
+
+/**
+ * hash_table_create - creates hash table
+ * @size: the size of the array
+ * Return: a new hastable on success, null on failure
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *newTable = NULL;
