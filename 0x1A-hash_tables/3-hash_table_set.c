7b6a5cfbfbccb3465def2828ce012c658b6968c4
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2023-06-09 10:03:57 +0300

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,7 +3,6 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	const unsigned char *unsignedKey;
 	int i = 0;
 
 	if (key == NULL || strcmp(key, "") == 0 || ht == NULL || value == NULL)
