bd3faede202c956a9eb6aa0508a1fb25537a8419
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-06-09 10:36:05 +0300

    changed size requirement

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 
 	if (ht->size == 0)
-		return (0);/* this could be (1) if it is considered a success*/
+		return (1);/* this could be (1) if it is considered a success*/
 	if (ht->array == NULL)
 		return (0);
 
