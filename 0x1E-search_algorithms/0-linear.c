2be78bb1cf0417820994dc2771748dff74c29c5d
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2024-07-17 03:23:34 +0300

    added comment

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,12 @@
 #include "search_algos.h"
 
+/**
+ * linear_search - searches for an int in an array in O(N)
+ * @array - array of integers
+ * @size - size of array
+ * @value -  value we are searching for
+ * Return: the idx or -1 on fail
+ */
 int linear_search(int *array, size_t size, int value)
 {
     if (array == NULL)
