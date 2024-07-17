cba7e953fd5a37e20fc691e2d5a6a0790819ab12
ahmedAhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2024-07-17 07:57:55 +0200

    betty style

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,20 +2,20 @@
 
 /**
  * linear_search - searches for an int in an array in O(N)
- * @array - array of integers
- * @size - size of array
- * @value -  value we are searching for
+ * @array: array of integers
+ * @size: size of array
+ * @value:  value we are searching for
  * Return: the idx or -1 on fail
  */
 int linear_search(int *array, size_t size, int value)
 {
-    if (array == NULL)
-        return -1;
-    for (size_t i = 0; i < size; i++)
-    {
-        printf("Value checked array[%zu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return i;
-    }
-    return -1;
+	if (array == NULL)
+		return (-1);
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("Value checked array[%zu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
